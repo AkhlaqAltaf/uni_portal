39d5f62c45fd42428d61cd2fac8b2e204a300643
Zainabkhan9118 <zainabkhan3473@gmail.com>
2024-12-24 18:00:05 +0500

    timetable made and adjusted

--- uni_portal_frontend/src/pages/TimeTable.jsx
+++ uni_portal_frontend/src/pages/TimeTable.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Schedule from '../components/widgets/Schedule';
+import Timetable from '../components/widgets/Timetable';
 
 const TimeTable = () => {
     return (
         <div className="p-6">
         {/* <h1 className="text-2xl font-bold mb-4">Time Table</h1> */}
             <div className="bg-white shadow-md rounded-lg p-4">
-                <Schedule />
+                <Timetable />
             </div>
         </div>
     );
