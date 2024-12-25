c75665dac6f6fd22b494aeab0efa4af7cca40144
Zainabkhan9118 <zainabkhan3473@gmail.com>
2024-12-25 15:28:31 +0500

    minor change

--- uni_portal_frontend/src/components/Header.jsx
+++ uni_portal_frontend/src/components/Header.jsx
@@ -4,13 +4,6 @@ const Header= () => {
     return (
         <header className="bg-blue-600 text-white p-4">
             <h1 className="text-xl font-bold">Student Portal</h1>
-            <nav>
-                <ul className="flex space-x-4">
-                    <li><a href="/" className="hover:underline">Home</a></li>
-                    <li><a href="/profile" className="hover:underline">Profile</a></li>
-                    <li><a href="/settings" className="hover:underline">Settings</a></li>
-                </ul>
-            </nav>
         </header>
     );
 };
