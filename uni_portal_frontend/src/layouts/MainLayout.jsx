a681f252a0f7fd277f263696cf7cece98c43059f
Zainabkhan9118 <zainabkhan3473@gmail.com>
2024-12-28 16:35:09 +0500

    changes made to the sidebar

--- uni_portal_frontend/src/layouts/MainLayout.jsx
+++ uni_portal_frontend/src/layouts/MainLayout.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import Header from '../components/teacher/common/Header';
-import Sidebar from '../components/student/common/Sidebar';
+import TeacherHeader from '../components/teacher/common/TeacherHeader';
+import StudentHeader from '../components/student/common/Header';
+import TeacherSidebar from '../components/teacher/common/TeacherSidebar';
+import StudentSidebar from '../components/student/common/Sidebar';
 
-const MainLayout = ({ children }) => {
+const MainLayout = ({ children, user }) => {
+    // Select components based on user role
+    const Header = user?.role === 'teacher' ? TeacherHeader : StudentHeader;
+    const Sidebar = user?.role === 'teacher' ? TeacherSidebar : StudentSidebar;
+
     return (
         <div className="flex flex-col h-screen">
-            <Header />
+            <Header user={user} />
             <div className="flex flex-1">
-                <Sidebar />
+                <Sidebar user={user} />
                 <main className="flex-1 p-4">
                     {children}
                 </main>
